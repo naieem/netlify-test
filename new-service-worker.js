01c5d670a2b5741f7a780934f9e62ea895bed568
Naieem Mahmud Supto <naieemsupto@gmail.com>
2020-08-04 23:28:36 +0600

    service worker new registration

--- new-service-worker.js
+++ new-service-worker.js
@@ -27,12 +27,23 @@ self.addEventListener('activate', event => {
     );
 });
 self.addEventListener('fetch', event => {
-    const url = new URL(event.request.url);
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    console.log('Found ', event.request.url, ' in cache');
+                    return response;
+                }
+                console.log('Network request for ', event.request.url);
+                return fetch(event.request)
 
-    // serve the cat SVG from the cache if the request is
-    // same-origin and the path is '/dog.svg'
-    // if (url.origin == location.origin && url.pathname == '/dog.svg') {
-    //     event.respondWith(caches.match('/cat.svg'));
-    // }
-    event.respondWith(url);
+                // TODO 4 - Add fetched files to the cache
+
+            }).catch(error => {
+
+                // TODO 6 - Respond with custom offline page
+
+            })
+    );
 });
